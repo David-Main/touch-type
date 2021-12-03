2012025db992c42e440ff3ab9e4387f8b313162a
David-Main <work.davidmainoo@gmail.com>
2021-12-03 18:51:02 +0000

    Added header element to app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import PageHeader from "./elements/Header";
+const App = () => {
+  return (
+    <div className="container container-app">
+      <PageHeader />
+    </div>
+  );
+};
+
+export default App;
