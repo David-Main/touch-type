366ad53d40c8874d6d8a17c9b762da5ebae29c2a
David-Main <work.davidmainoo@gmail.com>
2021-12-03 19:16:15 +0000

    Added metrics Section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import PageHeader from "./elements/Header";
+import Metric from "./components/metric";
+
 const App = () => {
   return (
     <div className="container container-app">
       <PageHeader />
+      <section className="section-metrics">
+        <Metric value={60} unit={"seconds"} />
+        <Metric value={0} unit={"words/min"} />
+        <Metric value={0} unit={"chars/min"} />
+      </section>
     </div>
   );
 };
