0c1df3323fbb9cfe92a270557782ee07b452c6e5
David-Main <work.davidmainoo@gmail.com>
2021-12-04 06:58:59 +0000

    iput Area functionality complete
    input area is now complete with formatting, reading and writing functionalities

--- src/components/words.js
+++ src/components/words.js
@@ -2,6 +2,11 @@ const Words = (props) => {
   return (
     <p className={`words  ${props.class}`}>
       {props.words.split(" ").map((word) => {
+        if (!word.indexOf("/*wrong*/")) {
+          return (
+            <span className="word wrong">{word.replace("/*wrong*/", "")}</span>
+          );
+        }
         return <span className="word">{word}</span>;
       })}
     </p>
--- src/containers/inputArea.js
+++ src/containers/inputArea.js
@@ -9,13 +9,15 @@ class InputArea extends Component {
     this.state = {
       activeWord: "",
       input: "",
-      validInput: true,
+      validInput: false,
       words: Sample,
+      typedWords: "",
     };
     this.handleChange = this.handleChange.bind(this);
     this.adjustInputSize = this.adjustInputSize.bind(this);
     this.validateInput = this.validateInput.bind(this);
     this.editWords = this.editWords.bind(this);
+    this.addTypedWord = this.addTypedWord.bind(this);
   }
   componentDidMount() {
     // Set vlaue of input to nothing;
@@ -26,7 +28,17 @@ class InputArea extends Component {
     this.setState({ activeWord: this.state.words.split(" ")[0] });
   }
   componentDidUpdate(prevProps, prevState) {
-    if (this.state.input !== prevState.input) {
+    if (
+      this.state.typedWords !== prevState.typedWords &&
+      this.state.input === prevState.input
+    ) {
+      //clear Input
+      this.setState({ validInput: false, input: "" });
+
+      // reset activeWord;
+      this.setState({ activeWord: this.state.words.split(" ")[1] });
+      // this.setState({ words: this.state.words.trimStart() });
+    } else if (this.state.input !== prevState.input) {
       // update size of input
       this.adjustInputSize();
 
@@ -47,13 +59,41 @@ class InputArea extends Component {
   editWords() {
     if (this.state.validInput) {
       this.setState({
-        words: words.slice(this.state.input.length),
+        words:
+          this.state.activeWord.slice(this.state.input.length) +
+          words.slice(
+            words.indexOf(this.state.activeWord) + this.state.activeWord.length
+          ),
+      });
+    }
+  }
+  addTypedWord() {
+    console.log("adding");
+    if (this.state.input.length) {
+      // input has value
+      if (this.state.input === this.state.activeWord) {
+        this.setState({
+          typedWords: this.state.typedWords + " " + this.state.input,
+        });
+      } else {
+        this.setState({
+          typedWords: `${this.state.typedWords} /*wrong*/${this.state.input}`,
+        });
+      }
+    } else {
+      // input is empty
+      this.setState({
+        typedWords: `${this.state.typedWords} /*wrong*/${this.state.activeWord}`,
       });
     }
   }
 
   handleChange(event) {
-    this.setState({ ...this.state, input: event.target.value });
+    if (event.target.value.at(-1) !== " ") {
+      this.setState({ ...this.state, input: event.target.value });
+    } else {
+      this.addTypedWord();
+    }
   }
 
   adjustInputSize() {
@@ -65,7 +105,7 @@ class InputArea extends Component {
   render() {
     return (
       <section className="input-area">
-        <Words class="typed" words={"this is the"} />
+        <Words class="typed" words={this.state.typedWords} />
         <input
           type="text"
           name="typing"
@@ -80,5 +120,4 @@ class InputArea extends Component {
     );
   }
 }
-
 export default InputArea;
